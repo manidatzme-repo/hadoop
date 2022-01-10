7e002aef2c9336f7467a51d69fbe649a88f05939
Manish Sharma <41021359+manidatzme-repo@users.noreply.github.com>
2022-01-10 22:01:36 +0530

    Update pyspark.py

--- pyspark.py
+++ pyspark.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import pyspark
 sc = pyspark.SparkContext()
-text_file = sc.textFile("hdfs:///tmp/abc.txt")
+text_file = sc.textFile("hdfs://localhost:9000/tmp/abc.txt")
 counts = text_file.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
-counts.saveAsTextFile("hdfs:///manish/output")
+counts.saveAsTextFile("hdfs://localhost:9000/manish/output")
